54e6ade9c41748de36a4be173ee8409f0bfafcb0
Jooan <sgh04069@likelion.org>
2019-06-10 21:17:35 +0900

    first commit of proj

---
+++ virtualvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gabriel\desktop\likelion\djangoproject\virtualvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ wordcount/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext'] #글 자체, 문자
+    words = text.split()
+    word_dic = {}
+    
+    for word in words:
+        if word in word_dic:
+            #increase
+            word_dic[word]+=1
+        else:
+            #add to dictionary
+            word_dic[word]=1
+
+    return render(request, 'result.html', {'full':text,'total':len(words),'dictionary':word_dic.items()})
